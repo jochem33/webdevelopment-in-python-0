426bdcde3aec05a6ee9cd3edf70093e45bacbcb6
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-04-19 09:53:18 +0200

    alles tot de opdracht van lesbrief 2

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 import time
 
 
 app = Flask(__name__)
 
+gameslijst = [
+    {
+        "naam": "Roblox",
+        "plaatje": "https://images.rbxcdn.com/6c27cb9db1779888868bf7d87e6d3709.jpg"
+    },
+    {
+        "naam": "Frotnite",
+        "plaatje": "https://i.ytimg.com/vi/rIJK8S2IFUc/hqdefault.jpg"
+    },
+    {
+        "naam": "Minecraft",
+        "plaatje": "https://minecraft.net/static/pages/img/minecraft-hero-og.c5517b7973e1.jpg"
+    }
+]
+
+@app.route("/games")
+def games():
+    return render_template("games.html", games = gameslijst)
+
+
 @app.route("/")
 def hello():
     return "GAMER DUB"
 
-@app.route("/contact")
-def contact():
-    return ("""Pietje Puk <br>
-                Flaskstraat 21 <br>
-                1092AD Amsterdam""")
+@app.route("/welcome/<name>")
+def contact(name):
+    return render_template("gamer.html", namePerson=name)
 
 @app.route("/tijd/<gamer>")
 def tijd(gamer):
-    return (time.strftime("%R") + " " + gamer)
+    return time.strftime("%R") + " " + gamer
 
 @app.route("/tafel/<int:tafel>/<string:otto>")
 def tafel(tafel, otto):
@@ -24,7 +42,7 @@ def tafel(tafel, otto):
     for i in range(1, 10):
         som = int(tafel) * i
         tafellijst = tafellijst + " " + str(som)
-    return (tafellijst + " " + str(otto))
+    return tafellijst + " " + str(otto)
 
 
 app.run(debug=True)
